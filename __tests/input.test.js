cd783f6b3a587d8e2ded32aa1e1bcce084314631
Joe Pennock <pennockjoe@gmail.com>
2020-08-13 14:09:09 -0700

    Created basic test framework for input.js tests. Added "jest" script to package.json to allow for tests to run.

---
+++ __tests/input.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* 
+TODOs:
+1. Write tests that will pass when user is providing valid input
+  a. validate() returns true
+  b. new instance of Input has both action and a payload
+2. Write tests that will pass when user provides invalid input
+  a. validate() returns false
+*/
+
+jest.mock('minimist');
+const minimist = require('minimist');
+
+minimist.mockImplementation( () => {
+  return {
+    // Filler params. Not yet sure what my mock data should be.
+    _: [],
+    // action: 'payload'
+    a: 'test',
+  };
+});
+
+const Input = require('../lib/input');
+
+describe('Test suite for the Input module from lib/input.js.', () => {
+  
+  it('Proof of life test.', () => {
+    let test = 'test';
+    expect(test).toEqual('test');
+  });
+
+  it('Verify that input is successfully being recieved from input.js module.', () => {
+    let options = new Input();
+    console.log(options);
+    expect(options.payload).toEqual('test');
+  });
+
+});
